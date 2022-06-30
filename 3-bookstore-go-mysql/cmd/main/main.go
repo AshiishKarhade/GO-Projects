495b4306fbd0510667bbcb56b205623f5b62f6ed
Ashish Karhade <akarhade5@gmail.com>
2022-06-30 11:24:40 +0530

    main function in go added

--- 3-bookstore-go-mysql/cmd/main/main.go
+++ 3-bookstore-go-mysql/cmd/main/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/AshiishKarhade/bookstore-go-mysql/pkg/routes"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
 func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
 
 }
