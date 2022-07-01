a5e7333a42290769a5262a597e08a500ae48722b
Ashish Karhade <akarhade5@gmail.com>
2022-07-01 16:33:15 +0530

    basic template done

--- 4-slackbot-api-go/go.mod
+++ 4-slackbot-api-go/go.mod
@@ -1,3 +1,11 @@
 module github.com/AshiishKarhade/slackbotapi-go
 
 go 1.18
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/shomali11/commander v0.0.0-20191122162317-51bc574c29ba // indirect
+	github.com/shomali11/proper v0.0.0-20180607004733-233a9a872c30 // indirect
+	github.com/shomali11/slacker v0.0.0-20220701072026-3318efb45757 // indirect
+	github.com/slack-go/slack v0.11.0 // indirect
+)
---
+++ 4-slackbot-api-go/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"github.com/shomali11/slacker"
+)
+
+func main() {
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-811591088449-3741496313510-91UXuZTKD7C9LCu08ne11vQw")
+	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A03NCMBQYQ1-3750493479844-d79322297369cb030060790284c951e2bd446666275cd7e4a3051382d0986641")
+
+	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+
+	go printCommandEvents(bot.CommandEvents())
+
+	bot.Command("my year of birth is <year>", &slacker.CommandDefinition) {
+		Description: "year of birth calculator", 
+		Example : "my year of birth is 1998",
+		Handler : func(botCtcx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+			year := request.Param("year")
+			yob, err := strconv.Atoi(year)
+		}
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := bot.Listen(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println("Command Events")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+		fmt.Println()
+	}
+}
