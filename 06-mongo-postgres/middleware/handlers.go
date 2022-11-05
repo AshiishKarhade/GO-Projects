16acc37d1555bafa52026ef7b9ac95055e163f3d
Ashish Karhade <akarhade5@gmail.com>
2022-11-05 18:32:27 +0530

    Postgres connection defined

--- 06-mongo-postgres/middleware/handlers.go
+++ 06-mongo-postgres/middleware/handlers.go
@@ -1,6 +1,37 @@
 package middleware
 
-import "net/http"
+import (
+	"database/sql"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
+
+type response struct {
+	ID      int    `json:"id",omitempty`
+	Message string `json:"message",omitempty`
+}
+
+func CreateConnection() *sql.DB {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	db, err := sql.Open("postgres", os.Getenv("POSTGRES_URL"))
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	
+	log.Println("Successfully connected to Postgres")
+	return db
+}
 
 func GetStock(w http.ResponseWriter, r *http.Request) {
 
