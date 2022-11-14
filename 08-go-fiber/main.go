01ea160f535e4dd9b5005195e0d1edce70085ace
Ashish Karhade <akarhade5@gmail.com>
2022-11-14 22:19:35 +0530

    DB initialisation completed

--- 08-go-fiber/main.go
+++ 08-go-fiber/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/AshiishKarhade/GO-Projects/go-fiber/database"
+	"github.com/gofiber/fiber"
+)
 
 func main() {
-	fmt.Println("Hello, World")
+	app := fiber.New()
+	initDB()
+	defer database.DBConn.Close()
+	setupRoutes(app)
+	app.Listen(3000)
+}
+
+func initDB() {
+	log.Println("Initialising DB...")
+}
+
+func setupRoutes(app *fiber.App) {
+	log.Println("Setting up routes...")
+	app.Get(GetLeads)
+	app.Get(GetLead)
+	app.Post(NewLead)
+	app.Delete(DeleteLead)
 }
